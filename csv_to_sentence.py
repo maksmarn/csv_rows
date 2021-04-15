88c155e7f8bd0900e702d1df0dde580ea62a9564
maksmarn <63792965+maksmarn@users.noreply.github.com>
2021-04-15 16:20:34 +0200

    A slightly corrected version.

--- csv_to_sentence.py
+++ csv_to_sentence.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('info.csv', 'r') as csv_file:
+with open('info.csv', newline='') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
